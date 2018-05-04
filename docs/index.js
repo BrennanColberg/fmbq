d1d4c76195e81529dac920a6eaf16febb088c630
Brennan Colberg <brennan.colberg@gmail.com>
2018-05-03 21:36:29 -0700

    Background Color Manipulation
    - linked JS file to click() and release() on body
    - click -> green background
    - release -> red background
    - removed that pointless alert
    - commented out Greet button

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,13 @@
+
+
 function greet() {
-	alert("index.js is connected");
+    alert("index.js is connected");
+}
+
+function click() {
+    document.backgroundColor = "4DFF9A";
+}
+
+function release() {
+    document.backgrounColor = "FF4D6B";
 }
