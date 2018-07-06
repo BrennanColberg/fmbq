23e414ff90ed2cd65cc8b93539ae96a63593333f
Brennan Colberg <brennan.colberg@gmail.com>
2018-07-05 20:39:55 -0700

    Restarting w/new knowledge
    I've been taking UW CSE154, realized how trash the website is, now am creating a new version (running from computer, to start)

--- docs/index.js
+++
@@ -1,7 +0,0 @@
-var sitting = false;
-var sittingColor = "40FF43";
-var standingColor = "FF4D6B";
-function toggleColor() {
-    document.bgColor = sitting ? sittingColor : standingColor;
-    sitting = !sitting;
-}
