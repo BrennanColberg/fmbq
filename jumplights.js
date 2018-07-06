9084fdf2ac2ea4a4009b122add0ed8b49207da37
Brennan Colberg <brennan.colberg@gmail.com>
2018-07-05 23:41:40 -0700

    Full Lights Functionality
    - name input
    - team name input
    - auto resizing
    - calculates jump order
    - progresses through jump order
    - resets jump order when everyone sits down
    - disables people without a name
    uses Q / A / Z on one side and ] / ' / / on the other side

---
+++ jumplights.js
@@ -0,0 +1,124 @@
+(function() {
+
+	// [ q a z ] vs [ ] ' / ] (sides)
+	const keys = [[81, 65, 90], [221, 222, 191]];
+	let reading = false;
+	let jumpTime = undefined;
+	let quizzers = [];
+	let standing = [];
+	let forfeited = [];
+	let asking = true;
+	
+	window.addEventListener("load", function() {
+		let lights = document.getElementById("lights");
+		for (let t = 0; t < keys.length; t++) {
+			let teamKeys = keys[t];
+			let team = document.createElement("div");
+			team.className = "team";
+			let teamName = document.createElement("input");
+			teamName.className = "name";
+			teamName.type = "text";
+			teamName.placeholder = "team";
+			team.appendChild(teamName);
+			lights.appendChild(team);
+			for (let i = 0; i < teamKeys.length; i++) {
+				let quizzer = document.createElement("div");
+				quizzer.className = "quizzer";
+				quizzer.active = false;
+				quizzer.keyCode = teamKeys[i];
+				// name field
+				let quizzerName = document.createElement("input");
+				quizzerName.className = "name";
+				quizzerName.type = "text";
+				quizzerName.team = t;
+				quizzerName.index = i;
+				quizzerName.onchange = updateName;
+				quizzerName.placeholder = "name";
+				quizzer.appendChild(quizzerName);
+				// order text
+				let message = document.createElement("h2");
+				message.innerHTML = "";
+				quizzer.appendChild(message);
+				// listeners
+				document.addEventListener("keyup", function(event){jump(event, quizzer)});
+				document.addEventListener("keydown", function(event){sit(event, quizzer)});
+				// append to team
+				team.appendChild(quizzer);
+				quizzers.push(quizzer);
+			}
+		}
+	});
+	
+	function getQuizzer(team, index) {
+		let lights = document.getElementById("lights");
+		let team = lights.childNodes[team];
+		let quizzer = team.childNodes[index];
+		return quizzer;
+	}
+	
+	function updateName() {
+		let quizzer = this.parentElement;
+		quizzer.active = this.value;
+		updateLight(quizzer);
+	}
+	
+	function updateLight(quizzer) {
+		if (quizzer.active) {
+			quizzer.className = "quizzer down";
+		} else {
+			quizzer.className = "quizzer";
+		}
+	}
+	
+	function jump(event, quizzer) {
+		if (event.keyCode == quizzer.keyCode && quizzer.active) {
+			quizzer.className = "quizzer up";
+			if (!forfeited.includes(quizzer)) {
+				enqueue(quizzer);
+			}
+		}
+	}
+	
+	function sit(event, quizzer) {
+		if (event.keyCode == quizzer.keyCode && quizzer.active) {
+			quizzer.className = "quizzer down";
+			dequeue(quizzer);
+		}
+	}
+	
+	function enqueue(quizzer) {
+		dequeue(quizzer);
+		standing.push(quizzer);
+		for (qi in quizzers) {
+			updateMessage(quizzers[qi]);
+		}
+	}
+	
+	function dequeue(quizzer) {
+		if (standing.includes(quizzer)) {
+			let index = standing.indexOf(quizzer);
+			standing.splice(index, 1);
+			forfeited.push(quizzer);
+		}
+		for (qi in quizzers) {
+			updateMessage(quizzers[qi]);
+		}
+		if (standing.length == 0) {
+			forfeited = [];
+		}
+	}
+	
+	function updateMessage(quizzer) {
+		let index = standing.indexOf(quizzer);
+		let message = "";
+		if (index == 0) {
+			message = "Answering";
+		} else if (index > 0 && index < 4) {
+			message = "#" + index + " in line";
+		} else {
+			message = "";
+		}
+		quizzer.lastChild.innerHTML = message;
+	}
+	
+})();
